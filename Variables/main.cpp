fa1838740a5d8fd677c7e29fede1cc155e713030
highwindsg <highwind.sg@gmail.com>
2022-03-20 16:19:08 +0800

    update file

--- Variables/main.cpp
+++ Variables/main.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "There once was a man named George" << endl;
-    cout << "He was 70 years old" << endl;
-    cout << "He liked the name George" << endl;
-    cout << "But did not like being 70" << endl;
+    string characterName = "Tom"; // Creating a var named 'characterName' of type string, and assign a string "John" to it.
+    int characterAge; // Declaring a 'characterAge' var of type 'int' (integer) for use later.
+    characterAge = 50; // Assigning integer number 35 to var 'characterAge'.
+    cout << "There once was a man named " << characterName << endl;
+    cout << "He was "<< characterAge << " years old" << endl;
+
+    characterName = "Mike"; // Changing the value of var 'characterName' mid-way, no need to declare type since it is still a string.
+    cout << "He liked the name " << characterName << endl;
+    cout << "But did not like being " << characterAge << endl;
     cout << endl;
 
 
