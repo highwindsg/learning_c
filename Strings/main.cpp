6914b6394dc8bdfdfe4cfac57105e11602f515de
highwindsg <highwind.sg@gmail.com>
2022-03-27 23:05:01 +0800

    new files

---
+++ Strings/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string phrase = "Giraffe Academy";
+    cout << "Giraffe Academy\n"; // a '\n' will print out a new line.
+    cout << phrase.length() << endl; // a '<< endl' will also means end line and start a new line.
+    cout << "Hello" << endl;
+
+    cout << phrase;
+
+
+    return 0;
+}
