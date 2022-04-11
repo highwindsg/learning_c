bd2bbcd9e7061adfeef7d9bfdee67c1557b515e4
highwindsg <highwind.sg@gmail.com>
2022-04-11 18:54:00 +0800

    updates

--- Strings/main.cpp
+++ Strings/main.cpp
@@ -6,8 +6,10 @@ int main()
 {
 
     string phrase = "Giraffe Academy";
+
     cout << "Giraffe Academy\n"; // a '\n' will print out a new line.
     cout << phrase.length() << endl; // a '<< endl' will also means end line and start a new line.
+    cout << phrase[0] << endl; // slicing on the first index which starts with 0 is letter G.
     cout << "Hello" << endl;
 
     cout << phrase;
