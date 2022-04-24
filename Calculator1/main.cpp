d5d228d5bea9dd46f2c6cc5b5b1fe323a43790a6
highwindsg <highwind.sg@gmail.com>
2022-04-24 20:47:19 +0800

    new files

---
+++ Calculator1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath> //  import module cmath for calculations.
+
+
+using namespace std;
+
+int main()
+{
+
+    double num1, num2; // Declaring two vars of type double which can take in numbers and decimals./
+    cout << "Enter first number: ";
+    cin >> num1;
+ 
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << num1 + num2;
+
+
+    return 0;
+
+}
