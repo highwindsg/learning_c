155f49684b32408057f3250206cfb40cc1bef5e3
highwindsg <highwind.sg@gmail.com>
2022-05-28 12:32:12 +0800

    updates

--- if_Statemts/main.cpp
+++ if_Statemts/main.cpp
@@ -9,8 +9,8 @@ int main()
     bool isMale = true; // Creating a var isMale of type boolean and set to true.
     bool isTall = false; // Creating a var isTall of type boolean and set to true.
 
-    //if(isMale && isTall){ // The condition inside the if() parentheses is isMale and isTall,
-    if(isMale || isTall){ // The condition inside the if() parentheses is isMale or isTall,
+    //if(isMale && isTall){ // The condition inside the if() parentheses is isMale 'and' isTall,
+    if(isMale || isTall){ // The condition inside the if() parentheses is isMale 'or' isTall,
         cout << "You are a tall male." << endl;
     } else {
         cout << "You are not a tall male." << endl;
