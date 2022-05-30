a5955a3af51251537e4b5830bd3b7a2e3f7193bf
highwindsg <highwind.sg@gmail.com>
2022-05-30 21:12:38 +0800

    new and updated files

--- if_Statemts/main.cpp
+++ if_Statemts/main.cpp
@@ -6,17 +6,39 @@ using namespace std;
 int main()
 {
 
-    bool isMale = true; // Creating a var isMale of type boolean and set to true.
-    bool isTall = false; // Creating a var isTall of type boolean and set to true.
+    bool isMale = false; // Creating a var isMale of type boolean.
+    bool isTall = false; // Creating a var isTall of type boolean.
 
-    //if(isMale && isTall){ // The condition inside the if() parentheses is isMale 'and' isTall,
-    if(isMale || isTall){ // The condition inside the if() parentheses is isMale 'or' isTall,
+    //if(isMale && isTall){ // The condition inside the if() parentheses is isMale AND isTall.
+    //if(isMale || isTall){ // The condition inside the if() parentheses is isMale OR isTall.
+    if(isMale && isTall) { // The condition inside the if() parentheses is isMale AND isTall. False AND False is False.
         cout << "You are a tall male." << endl;
+    } else if(isMale && !isTall) {      // False AND Not False is False.
+        cout << "You are a short male." << endl;
+    } else if(!isMale && isTall) {      // Not False AND False is False.
+        cout << "You are tall but not a male." << endl;
     } else {
-        cout << "You are not a tall male." << endl;
+        cout << "You are not a male and not tall." << endl;
     }
 
 
     return 0;
 }
 
+/*
+
+Boolean AND / && :
+
+true && true is true
+true && false is false
+false && true is false
+false && false is false
+
+Boolean OR / || :
+
+true || true is true
+true || false is true
+false || true is true
+false || false is false
+
+*/
