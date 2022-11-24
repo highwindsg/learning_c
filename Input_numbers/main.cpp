2315bae7c44075717fca24a3dc836bc4d3030d01
highwindsg <highwind.sg@gmail.com>
2022-11-24 21:13:55 +0800

    updates

--- Input_numbers/main.cpp
+++ Input_numbers/main.cpp
@@ -8,11 +8,16 @@ int main()
 {
 
     double age; // Declaring var 'age' as type double, to include decimal numbers.
+    string country; // Declaring var 'country' as type character.
 
     cout << "Enter your age: "; // Print out a line to ask user to enter their age.
     cin >> age; // 'cin' and two greater than sign, will store the user's input into the var 'age'.
 
+    cout << "Which country do you come from: ";
+    cin >> country;
+
     cout << "You are " << age << " years old." << endl;
+    cout << "And you are from " << country << "." << endl;
 
     return 0;
 
--- Input_strings/main.cpp
+++ Input_strings/main.cpp
@@ -8,12 +8,18 @@ int main()
 {
 
     string name; // Declaring var 'name' as a string.
+    string address; // Declaring var 'address' as a string.
+
     cout << "Enter your name: "; // Print out a line to ask user to enter their name.
     /* The getline() func will take in all numbers or characters entered in a string.
     And parse in two params of 'cin' to store in the user's input into var 'name' */
     getline(cin, name);
 
+    cout << "Please enter your full mailing address: ";
+    getline(cin, address);
+
     cout << "Hello " << name << endl;
+    cout << "And you address is " << address << endl;
 
     return 0;
     
